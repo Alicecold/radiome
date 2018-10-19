f7c9baa66654eeaf539c7579e8ab5034c26076f9
Alice Darner <alice.darner@gmail.com>
2018-10-19 07:32:15 +0200

    pre-hack-friday

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+
+/* Navigo routing - defaults */
+var root = null;
+var useHash = false;
+var hash = '#';
+var router = new Navigo(root, useHash, hash);
+
 document.getElementById('login_btn').onclick = function(){
     fetch('http://localhost:8080/')
     .then(function(response) {
@@ -7,3 +14,16 @@ document.getElementById('login_btn').onclick = function(){
       console.log(JSON.stringify(json));
     });
 }
+
+router
+.on({'/:userid':  {
+as: 'play.show', uses: () => {
+  console.log('hejsan');
+}
+  },
+  '*': function () {
+    console.log('main')
+    }
+  }).resolve();
+
+  console.log(router.generate('play.show', {userid: 'alicecold'}));
